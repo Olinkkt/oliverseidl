0b3d8b0dffedc651d12c38e990f12255a3dd3da9
Oliver Seidl <seidl.oliver@icloud.com>
2024-12-24 01:09:50 +0100

    Update Next.js configuration and package dependencies: removed experimental CSS optimization from next.config.mjs, added 'critters' package for improved performance in package.json and package-lock.json, and included additional dependencies for enhanced functionality and maintainability.

--- next.config.mjs
+++ next.config.mjs
@@ -11,10 +11,7 @@ const nextConfig = {
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
-  },
-  experimental: {
-    optimizeCss: true,
-  },
+  }
 }
 
 export default nextConfig
